03d695a6727f30b2c8a6ce2a63fe53e7de177866
Hung Nguyen <hello@hvng.me>
2021-08-04 23:41:00 -0400

    update docstring

--- analytics.py
+++ analytics.py
@@ -3,7 +3,16 @@ from typing import List
 
 import numpy as np
 
-def sum_deviations_squared(a: List[float], b: List[float]):
+def sum_deviations_squared(a: List[float], b: List[float]) -> float:
+    """Calculate sum of deviations squared
+
+    Args:
+        a (List[float]): Value list A
+        b (List[float]): Value list B
+
+    Returns:
+        float: sum of deviations squared
+    """
     a = np.array(a)
     b = np.array(b)
     return np.sum((a - b) ** 2)
