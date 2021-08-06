e6af44c8a14ea6abda81e65ad9137039624826d5
Hung Nguyen <hello@hvng.me>
2021-08-05 22:38:39 -0400

    add analytics function

--- analytics.py
+++ analytics.py
@@ -1,7 +1,6 @@
 from typing import List
-
-
 import numpy as np
+import pandas as pd
 
 def sum_deviations_squared(a: List[float], b: List[float]) -> float:
     """Calculate sum of deviations squared
@@ -16,3 +15,14 @@ def sum_deviations_squared(a: List[float], b: List[float]) -> float:
     a = np.array(a)
     b = np.array(b)
     return np.sum((a - b) ** 2)
+
+def list_to_dataframe(lst: List[dict]) -> pd.DataFrame:
+    columns = {}
+
+    for item in lst:
+        for column in item.keys():
+            if column not in columns:
+                columns[column] = []
+            columns[column].append(item[column])
+
+    return pd.DataFrame(data=columns)
--- tst/analytics_test.py
+++ tst/analytics_test.py
@@ -1,5 +1,5 @@
 import unittest
-from analytics import sum_deviations_squared
+from analytics import sum_deviations_squared, list_to_dataframe
 
 
 class AnalyticsTests(unittest.TestCase):
@@ -9,3 +9,14 @@ class AnalyticsTests(unittest.TestCase):
 
         expected = (2 - 1) ** 2 + (5 - 8) ** 2 + (11 - 3) ** 2
         self.assertEqual(expected, sum_deviations_squared(a, b))
+
+    def test_list_to_dataframe(self):
+        lst = [
+            {'a': 1, 'b': 2},
+            {'a': 3, 'b': 4},
+        ]
+        expected = {
+            'a': {0: 1, 1: 3},
+            'b': {0: 2, 1: 4}
+        }
+        self.assertEqual(expected, list_to_dataframe(lst).to_dict())
